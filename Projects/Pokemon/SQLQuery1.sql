7cef9a0115a97ee2e88c03dfb6f5765e4dfbc040
AnupJacob <anupjacob2017@gmail.com>
2025-03-03 19:13:33 +0000

    Adding the initial queries to create a database for pokemon

---
+++ Projects/Pokemon/SQLQuery1.sql
@@ -0,0 +1,12 @@
+SELECT * FROM [dbo].[POKEMONTYPES];
+SELECT * FROM [dbo].[Pokemon];
+
+
+delete from [dbo].[POKEMONTYPES];
+
+delete from [dbo].[Pokemon];
+
+
+drop table [dbo].[POKEMONTYPES];
+drop table [dbo].[Pokemon];
+
